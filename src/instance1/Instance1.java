b4e0930005e20b2553093df6511d758e6c0f13cc
ikky225 <colombearthur@hotmail.fr>
2014-10-08 01:53:43 -0400

    premier commit

---
+++ src/instance1/Instance1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package instance1;
+
+/**
+ *
+ * @author colom_000
+ */
+public class Instance1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
